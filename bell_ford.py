bc3dfea73201fd9c55a5bb1d51d6010d17ecc6e5
Grey Harris <gharris@localhost.localdomain>
2020-05-05 01:37:20 +1200

    dded code from bell ford, havent tested it but wrote off of psudo code from some jurnal so should be okay, linked journal in header notes

---
+++ bell_ford.py
@@ -0,0 +1,31 @@
+"""
+Bellman Ford routing algorithm to help with RIP protocol assignment
+inspiration from https://iopscience.iop.org/article/10.1088/1742-6596/1007/1/012009/pdf
+"""
+
+INFINITY = float("inf") #positive infinity
+
+def bell_ford(verts, edges, current):
+    dist = []
+    prev = []
+
+    for index, this in enumerate(verts):
+        if current == this:
+            dist[index] = 0
+        else:
+            dist[index] = INFINITY
+            prev[index] = None
+
+    for i in range(1, len(verts) - 1):
+        for edge in edges:
+            u, v, w = edge
+            if (dist[u] + w) < dist[v]:
+                dist[v] = dist[u] + w
+                prev[v] = u
+
+    for edge in edges:
+        u, v, w = edge
+        if (dist[u] + w) < dist[v]:
+            return ValueError("negative weight cycle: broken")
+
+    return dist, prev
