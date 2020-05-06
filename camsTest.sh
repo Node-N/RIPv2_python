b5cbf02d4c53f8304458dcea421eafe7489ea8da
Grey Harris <gharris@localhost.localdomain>
2020-05-07 00:18:14 +1200

    added old test as well

--- camsTest.sh
+++ camsTest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-gnome-terminal -- /bin/sh -c "python3 rip.py config.txt; exec bash" &
-gnome-terminal -- /bin/sh -c  "python3 rip.py config2.txt; exec bash" &
-gnome-terminal -- /bin/sh -c  "python3 rip.py config3.txt; exec bash" &
-gnome-terminal -- /bin/sh -c  "python3 rip.py config4.txt; exec bash" &
-gnome-terminal -- /bin/sh -c  "python3 rip.py config5.txt; exec bash"
+gnome-terminal -- /bin/sh -c  "python3 rip.py tests/camsTest/config.txt; exec bash" &
+gnome-terminal -- /bin/sh -c  "python3 rip.py tests/camsTest/config2.txt; exec bash" &
+gnome-terminal -- /bin/sh -c  "python3 rip.py tests/camsTest/config3.txt; exec bash" &
+gnome-terminal -- /bin/sh -c  "python3 rip.py tests/camsTest/config4.txt; exec bash" &
+gnome-terminal -- /bin/sh -c  "python3 rip.py tests/camsTest/config5.txt; exec bash"
